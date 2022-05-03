a534a3ade260699e3753765729ab1f8de3f76145
Evan <81766815+Evan507@users.noreply.github.com>
2022-05-03 19:38:55 -0400

    basically done

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -2,6 +2,7 @@ import { gsap } from "gsap";
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 
+
 //  Register Pluging
 gsap.registerPlugin(DrawSVGPlugin); 
 gsap.registerPlugin(MorphSVGPlugin);
