45dd81d540f57a828037e0fea8922fc8ce106e40
Evan <81766815+Evan507@users.noreply.github.com>
2022-05-17 12:17:54 -0400

    Basic Bubble Grow

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -8,4 +8,9 @@ gsap.registerPlugin(DrawSVGPlugin);
 gsap.registerPlugin(MorphSVGPlugin);
 
 const svganim = gsap.timeline ()
+
+svganim
+.from(".bubble1", {scale: 0, duration:2.5, ease: "elastic.out(1.5, 0.5)"}, "grow1")
+.from(".bubble2", {scale: 0, duration:2.5, ease: "elastic.out(1.25, 0.5)"}, "grow2")
+.from(".bubble3", {scale: 0, duration:2.5, ease: "elastic.out(1, 0.5)"}, "grow3")
 ;
