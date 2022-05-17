bc3ee861c8f0cb0b4468bbf7d0db5bc2da5661b2
Evan <81766815+Evan507@users.noreply.github.com>
2022-05-17 16:50:55 -0400

    bubble animation done

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -7,10 +7,26 @@ import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 gsap.registerPlugin(DrawSVGPlugin); 
 gsap.registerPlugin(MorphSVGPlugin);
 
-const svganim = gsap.timeline ()
+const svganim = gsap.timeline ({repeat: -1})
 
 svganim
 .from(".bubble1", {scale: 0, duration:2.5, ease: "elastic.out(1.5, 0.5)", transformOrigin:'50% 50%'}, "grow1")
 .from(".bubble2", {scale: 0, duration:2.5, ease: "elastic.out(1.25, 0.5)", transformOrigin:'50% 50%'}, "grow2")
 .from(".bubble3", {scale: 0, duration:2.5, ease: "elastic.out(1, 0.5)", transformOrigin:'50% 50%'}, "grow3")
+.to(".bubble1", {ease: "elastic.out(1.5, 0.5)", transformOrigin:'50% 50%', scale:1.2}, "balloon1")
+.to(".bubble1", {opacity: 0, delay: 0, duration: .01}, "gone1")
+.from(".pop1", {opacity:0, duration: .02}, "pop1")
+.to(".bubble2", {ease: "elastic.out(1.5, 0.5)", transformOrigin:'50% 50%', scale:1.2}, "balloon2")
+.to(".bubble2", {opacity: 0, delay: 0, duration: .01}, "gone2")
+.from(".pop2", {opacity:0, duration: .02}, "pop2")
+.to(".bubble3", {ease: "elastic.out(1.5, 0.5)", transformOrigin:'50% 50%', scale:1.2}, "balloon3")
+.to(".bubble3", {opacity: 0, delay: 0, duration: .01}, "gone3")
+.from(".pop3", {opacity:0, duration: .02}, "pop3")
+.to(".pop1", {opacity:0, duration:1}, "fadeffect")
+.to(".pop2", {opacity:0, duration:1}, "fadeffect")
+.to(".pop3", {opacity:0, duration:1}, "fadeffect")
+
+
+
+
 ;
